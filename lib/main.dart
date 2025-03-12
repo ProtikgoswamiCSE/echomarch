22e035bc4dbc6f9ffe14ed3b4e3e2391adf1a6fe
ProtikgoswamiCSE <goswami15-5841@diu.edu.bd>
2025-03-12 10:08:56 +0600

    basic_scliton

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
+import 'package:echomarch/screen/home/page/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FreebiesEcco());
 }
 
+class FreebiesEcco extends StatefulWidget {
+  const FreebiesEcco({super.key});
+
+  @override
+  State<FreebiesEcco> createState() => _FreebiesEccoState();
+}
+
+class _FreebiesEccoState extends State<FreebiesEcco> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "E-co", home: Home());
+  }
+}
---
+++ lib/utils/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class FColor {
+  static const primary = Color(0xFF5CC936);
+  static const secondary = Color(0xFFFFC120);
+  static const danger = Color(0xFF0C1A30);
+  static const white = Color(0xFFFFFFFF);
+}
